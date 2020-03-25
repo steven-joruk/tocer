d8e5d0e89dc7cf39fe7a1cde7b3ccfe519a768c5
Steven Joruk <steven@joruk.com>
2020-03-25 21:33:40 +0000

    Remove Regex, just use a simple manual parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,51 @@
-use regex::Regex;
 use std::collections::HashMap;
 use std::io::{BufRead, BufReader};
-
-pub type Result<T> = std::result::Result<T, TocError>;
-
-#[derive(Debug)]
-pub enum TocError {
-    RegexCompilationError(String),
-    ReadLineError(String),
-}
-
-impl std::fmt::Display for TocError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            TocError::RegexCompilationError(s) => write!(f, "{}", s),
-            TocError::ReadLineError(s) => write!(f, "{}", s),
-        }
-    }
-}
-
-impl std::convert::From<regex::Error> for TocError {
-    fn from(e: regex::Error) -> Self {
-        Self::RegexCompilationError(e.to_string())
-    }
-}
-
-impl std::convert::From<std::io::Error> for TocError {
-    fn from(e: std::io::Error) -> Self {
-        Self::ReadLineError(e.to_string())
-    }
-}
 
 pub struct Toc {
     pub tags: HashMap<String, String>,
     pub files: Vec<String>,
+}
+
+fn key_value_pair_begin(input: &str) -> Result<&str, &str> {
+    let mut chars = input.chars();
+    if chars.next() == Some('#') && chars.next() == Some('#') {
+        Ok(&input[2..])
+    } else {
+        Err(input)
+    }
+}
+
+fn key(input: &str) -> Result<(&str, &str), &str> {
+    for (i, ch) in input.chars().enumerate() {
+        if ch == ':' {
+            let key = input[..i].trim();
+            return Ok((key, &input[i..]));
+        }
+    }
+
+    Err(input)
+}
+
+fn value(input: &str) -> Result<&str, &str> {
+    if input.chars().next() != Some(':') {
+        Err(input)
+    } else {
+        Ok(&input[1..].trim())
+    }
+}
+
+fn key_value_pair(input: &str) -> Result<(&str, &str), &str> {
+    let (k, input) = key(key_value_pair_begin(input)?)?;
+    let v = value(input)?;
+    Ok((k, v))
+}
+
+fn file_path(input: &str) -> Result<&str, &str> {
+    if input.chars().next() == Some('#') {
+        Err(input)
+    } else {
+        Ok(input.trim())
+    }
 }
 
 impl Toc {
@@ -45,30 +57,22 @@ impl Toc {
     /// ```
     /// use tocer::Toc;
     /// let reader = std::io::Cursor::new("## Interface: 1");
-    /// let toc = Toc::load(reader).unwrap();
+    /// let toc = Toc::from_reader(reader).unwrap();
     /// dbg!(&toc.tags["Interface"]);
     /// ```
-    pub fn load(reader: impl std::io::Read) -> Result<Toc> {
+    pub fn from_reader(reader: impl std::io::Read) -> std::io::Result<Toc> {
         let mut buf = BufReader::new(reader);
         let mut line = String::new();
         let mut tags = HashMap::new();
         let mut files = Vec::new();
 
-        let tag_re = Regex::new(r"^##\s*(\S+)\s*:\s*(.*)")?;
-        let file_re = Regex::new(r"^([^#\s].*)")?;
-
-        // The unwraps can't fail without breaking the regex pattern, which
-        // would also break the tests.
         while buf.read_line(&mut line)? != 0 {
-            if let Some(caps) = tag_re.captures(line.as_ref()) {
-                let tag = caps.get(1).unwrap();
-                let value = caps.get(2).unwrap();
-                tags.insert(tag.as_str().into(), value.as_str().into());
-            } else if let Some(caps) = file_re.captures(line.as_ref()) {
-                let file = caps.get(1).unwrap();
-                files.push(file.as_str().into());
+            if let Ok((k, v)) = key_value_pair(&line) {
+                tags.insert(k.to_string(), v.to_string());
+            } else if let Ok(path) = file_path(&line) {
+                println!("Adding {}", path);
+                files.push(path.to_string());
             }
-
             line.clear();
         }
 
@@ -81,9 +85,38 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_key_begin() {
+        assert_eq!(key_value_pair_begin(""), Err(""));
+        assert_eq!(key_value_pair_begin("#"), Err("#"));
+        assert_eq!(key_value_pair_begin("##"), Ok(""));
+    }
+
+    #[test]
+    fn test_key() {
+        assert_eq!(key(""), Err(""));
+        assert_eq!(key("a"), Err("a"));
+        assert_eq!(key(" a "), Err(" a "));
+        assert_eq!(key("a:"), Ok(("a", ":")));
+        assert_eq!(key(" a:"), Ok(("a", ":")));
+        assert_eq!(key("a:"), Ok(("a", ":")));
+        assert_eq!(key("a :"), Ok(("a", ":")));
+        assert_eq!(key(" a :"), Ok(("a", ":")));
+    }
+
+    #[test]
+    fn test_value() {
+        assert_eq!(value(""), Err(""));
+        assert_eq!(value(" "), Err(" "));
+        assert_eq!(value(": "), Ok(""));
+        assert_eq!(value(":A"), Ok("A"));
+        assert_eq!(value(": A"), Ok("A"));
+        assert_eq!(value(": A "), Ok("A"));
+    }
+
+    #[test]
     fn test_empty_reader() {
         let reader = std::io::Cursor::new("");
-        let toc = Toc::load(reader).unwrap();
+        let toc = Toc::from_reader(reader).unwrap();
         assert!(toc.tags.is_empty());
         assert!(toc.files.is_empty());
     }
@@ -100,13 +133,14 @@ mod tests {
             " f"
         );
         let reader = std::io::Cursor::new(s);
-        let toc = Toc::load(reader).unwrap();
+        let toc = Toc::from_reader(reader).unwrap();
         assert!(toc.tags.is_empty());
-        assert_eq!(toc.files.len(), 4);
+        assert_eq!(toc.files.len(), 5);
         assert_eq!(toc.files[0], "a.lua");
         assert_eq!(toc.files[1], "b.lua");
-        assert_eq!(toc.files[2], "c.lua  ");
+        assert_eq!(toc.files[2], "c.lua");
         assert_eq!(toc.files[3], "dir\\d.xml");
+        assert_eq!(toc.files[4], "f");
     }
 
     #[test]
@@ -122,13 +156,13 @@ mod tests {
             "##   \t OptionalDeps : BagBrother, WoWUnit\n"
         );
         let reader = std::io::Cursor::new(s);
-        let toc = Toc::load(reader).unwrap();
+        let toc = Toc::from_reader(reader).unwrap();
         assert_eq!(toc.tags.len(), 6);
         assert_eq!(toc.tags["Interface"], "11302");
         assert_eq!(toc.tags["Title"], "|cff20ff20Bagnon|r");
         assert_eq!(toc.tags["Author"], "Tuller & Jaliborc (João Cardoso)");
         assert_eq!(toc.tags["d"], "8.2.16");
-        assert_eq!(toc.tags["e"], "Bagnon_Sets ");
+        assert_eq!(toc.tags["e"], "Bagnon_Sets");
         assert_eq!(toc.tags["OptionalDeps"], "BagBrother, WoWUnit");
         assert!(toc.files.is_empty());
     }
